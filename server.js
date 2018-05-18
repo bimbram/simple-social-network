4b1a31ba89fe9e962e1ad01770b9f34276a470ac
Bimo Bramantyo <bim.bramantyo@gmail.com>
2018-05-18 15:31:33 +0700

    establishing connection to database

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// DB Config
+const db = require('./config/keys.js').mongoURI;
+
+// Connect to MongoDB
+mongoose.connect(db, (err) => {
+  if (err) {
+    console.log("can't connect to database \n", err);
+  }
+  console.log("connected to database");
+});
 
 app.get('/', (req, res) => {
   res.send('hello');
