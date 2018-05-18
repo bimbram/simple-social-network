f236775b2beeb503c11669ecd61f0ef5c8f3157d
Bimo Bramantyo <bim.bramantyo@gmail.com>
2018-05-18 17:49:59 +0700

    adding validation handler using validator library

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,6 +5,10 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
+
+// Load Input Validation
+const validateRegisterInput = require('../../validation/register');
+
 // Load User model
 const User = require('../../models/User');
 
@@ -19,6 +23,13 @@ router.get('/test', (req, res) => {
 // @desc    Register user
 // @access  Public
 router.post('/register', (req, res) => {
+  const {errors, isValid} = validateRegisterInput(req.body);
+
+  // Check validation
+  if(!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email })
     .then(user => {
       if(user) {
