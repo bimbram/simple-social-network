3bb27fa0d2f30e82b47b3a708159ffa04694e269
Bimo Bramantyo <bim.bramantyo@gmail.com>
2018-05-18 15:46:43 +0700

    creating the posts, profile, and posts routes

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+
 const app = express();
 
 // DB Config
@@ -17,6 +22,12 @@ mongoose.connect(db, (err) => {
 app.get('/', (req, res) => {
   res.send('hello');
 });
+
+// User Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
 
 const PORT = process.env.PORT | 3000;
 
