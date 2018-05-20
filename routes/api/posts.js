41422b399394343d749fd7839ea8b41f03c10847
Bimo Bramantyo <bim.bramantyo@gmail.com>
2018-05-20 19:26:05 +0700

    adding new routes: add and delete comments

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -132,9 +132,62 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
   });
 
   newPost.save().then(post => res.json(post));
-
 });
 
+
+// @route   POST api/posts/comment/:id
+// @desc    Add a comment to post
+// @access  Private
+router.post('/comment/:post_id', passport.authenticate('jwt', { session: false}), (req , res) => {
+  const { errors, isValid } = validatePostInput(req.body);
+
+  // Check Validation
+  if(!isValid) {
+    // If any errors, send 400 with errors ObjectId
+    return res.status(400).json(errors);
+  }
+
+
+  Post.findById(req.params.post_id)
+    .then(post => {
+      const newComment = {
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.avatar,
+        user: req.user.id
+      }
+
+      // Add to comments array
+      post.comments.unshift(newComment);
+
+      // Save
+      post.save().then(post => res.json(post))
+        .catch(err => res.status(404).json({ postnotfound: 'No post found'}));
+    });
+});
+
+
+// @route   DELETE api/posts/comment/:id/:comment_id
+// @desc    Delete a comment from post
+// @access  Private
+router.delete('/comment/:post_id/:comment_id', passport.authenticate('jwt', { session: false}), (req , res) => {
+  Post.findById(req.params.post_id)
+    .then(post => {
+      // Check to see if comment exists
+      if(post.comments.filter(comment => comment._id.toString() === req.params.comment_id).length === 0) {
+        return res.status(404).json({ commentnotexists: 'Comment does not exist' });
+      }
+
+      // Get remove index
+      const removeIndex = post.comments
+        .map(item => item._id.toString())
+        .indexOf(req.params.comment_id)
+
+      post.comments.splice(removeIndex, 1);
+      post.save().then(post => res.json(post));
+    })
+    .catch(err => res.status(404).json({ postnotfound: 'no post found' }));
+});
 
 
 module.exports = router;
